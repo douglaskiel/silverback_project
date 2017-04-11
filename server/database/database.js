73766493022799e6a924c8647f2015f95b67ee57
Chris Beckman <atadsp@gmail.com>
2017-04-11 10:51:18 -0400

    changed connection string

--- server/database/database.js
+++ server/database/database.js
@@ -3,13 +3,15 @@ var assert = require('assert');
 
 var Sequelize = require('sequelize');
 
-var connection = new Sequelize(process.env.DB, process.env.DBUser, process.env.DBPass, {
-	dialect: 'postgres',
-	dialectOptions: {
-		// ssl: {
-		// 	require: true
-		// }
-	}
-});
+// var connection = new Sequelize(process.env.DB, process.env.DBUser, process.env.DBPass, {
+// 	dialect: 'postgres',
+// 	dialectOptions: {
+// 		// ssl: {
+// 		// 	require: true
+// 		// }
+// 	}
+// });
+
+var connection = new Sequelize(process.env.DATABASE_URL);
 
 module.exports = connection;
