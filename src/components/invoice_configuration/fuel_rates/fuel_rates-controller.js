452753337b6accc53900313eba2659b9028390a9
Chris Beckman <atadsp@gmail.com>
2017-04-17 15:00:35 -0400

    date fix

--- src/components/invoice_configuration/fuel_rates/fuel_rates-controller.js
+++ src/components/invoice_configuration/fuel_rates/fuel_rates-controller.js
@@ -74,7 +74,7 @@
 				.then(function(response) {
 					$scope.allFuelRates = response.data.data;
 					for (var i = 0; i < $scope.allFuelRates.length; i++) {
-						$scope.allFuelRates[i].fuel_date = new Date.getUTCDate($scope.allFuelRates[i].fuel_date);
+						$scope.allFuelRates[i].fuel_date = new Date (Date.parse($scope.allFuelRates[i].fuel_date));
 					}
 
 				}, function(err) {
