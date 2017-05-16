924e97012c0d541fefe054cfa444cbb97117d75d
Chris Beckman <atadsp@gmail.com>
2017-05-16 13:00:34 -0400

    added password checking to profile page

--- src/components/user/controller/profile-controller.js
+++ src/components/user/controller/profile-controller.js
@@ -9,6 +9,15 @@
 			};
 
 			$scope.user = {};
+			$scope.passLength = false;
+			$scope.special = false;
+			$scope.upperCase = false;
+			$scope.lowerCase = false;
+			$scope.number = false;
+			$scope.requirements = false;
+			$scope.passMatch = false;
+			$scope.errors = false;
+			$scope.errorsArry = [];
 
 			$scope.submitProfile = function(user) {
 				$http.put('/secure-api/user/update_profile', user, config)
@@ -34,8 +43,58 @@
 				}
 			};
 
+			$scope.passCheck = function(newPass, confirmPass) {
+				var specialTest = /[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/;
+				var upperCaseTest = /[A-Z]/;
+				var lowerCaseTest = /[a-z]/;
+				var numberTest = /\d/;
+
+				if (newPass.length > 13) {
+					$scope.passLength = true;
+				} else if (newPass.length < 14) {
+					$scope.passLength = false;
+				}
+
+				if (specialTest.test(newPass)) {
+					$scope.special = true;
+				} else {
+					$scope.special = false;
+				}
+
+				if (upperCaseTest.test(newPass)) {
+					$scope.upperCase = true;
+				} else {
+					$scope.upperCase = false;
+				}
+
+				if (lowerCaseTest.test(newPass)) {
+					$scope.lowerCase = true;
+				} else {
+					$scope.lowerCase = false;
+				}
+
+				if (numberTest.test(newPass)) {
+					$scope.number = true;
+				} else {
+					$scope.number = false;
+				}
+
+				if (newPass === confirmPass) {
+					$scope.passMatch = true;
+				} else {
+					$scope.passMatch = false;
+				}
+
+				if ($scope.passMatch === true && $scope.passLength === true && $scope.special === true && $scope.upperCase === true && $scope.lowerCase === true && $scope.number === true) {
+					$scope.requirements = true;
+				} else {
+					$scope.requirements = false;
+				}
+
+			};
+
 			$scope.changePassword = function(user) {
-				if (user.new_password === user.confirm_password) {
+				if ($scope.requirements === true) {
 					$http.put('/secure-api/user/change_password', user, config)
 						.then(function(response) {
 							console.log('User Updated');
@@ -48,7 +107,10 @@
 							}
 						});
 				} else {
-					console.log('Passwords Do Not Match');
+					console.log('Passwords Requirments Not Met');
+					$scope.errors = true;
+					$scope.errorsArry = [];
+					$scope.errorsArry.push('Passwords Requirments Not Met');
 				}
 			};
 
